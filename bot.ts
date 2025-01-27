a51fc36b39a3bb44c2a968649c4704866d9d7db3
Hayk Khachatryan <hi@hayk.io>
2025-01-27 18:11:45 +0400

    feat: update bot response

--- bot.ts
+++ bot.ts
@@ -2,6 +2,7 @@ import { Telegraf } from "telegraf";
 import type { Context } from "telegraf";
 import * as chrono from "chrono-node";
 import { Markup } from "telegraf";
+import { Message } from "../../Library/Caches/deno/npm/registry.npmjs.org/@telegraf/types/7.1.0/message.d.ts";
 
 class Bot {
   bot: Telegraf;
@@ -33,19 +34,79 @@ class Bot {
           // * no args
           this.sendSnarkyResponse({ ctx, responseType: "noArgs" });
         } else {
-          const date = chrono.parseDate(args);
+          const end = chrono.parseDate(args)?.toTemporalInstant();
 
-          if (!date) {
+          if (!end) {
             // * arg not a date
             await this.sendSnarkyResponse({
               ctx,
               responseType: "notUnderstood",
             });
           } // * yay we have a date
-          else await ctx.reply(`countdown registered ${date}`);
+          else {
+            const msg = await ctx.reply(`countdown registered ${end}`);
+            this.bot.telegram.pinChatMessage(msg.chat.id, msg.message_id);
+
+            this.startCountdown(msg, end);
+          }
         }
       },
     );
+  };
+
+  private startCountdown = (msg: Message, end: Temporal.Instant) => {
+    const intervalId = setInterval(() => {
+      const diff = this.getDifference(end);
+
+      if (diff.seconds <= 0) {
+        clearInterval(intervalId);
+        return;
+      }
+
+      const diffString = this.getDurationString(diff);
+
+      this.bot.telegram.editMessageText(
+        msg.chat.id,
+        msg.message_id,
+        undefined,
+        `${diffString} left`,
+      );
+    }, 1000);
+  };
+
+  private getDifference = (end: Temporal.Instant) => {
+    const now = Temporal.Now.instant();
+
+    return now.until(end);
+  };
+
+  private getDurationString = (duration: Temporal.Duration) => {
+    const totalSeconds = duration.total("seconds");
+    const years = Math.floor(totalSeconds / (365 * 24 * 60 * 60));
+    const months = Math.floor(
+      (totalSeconds % (365 * 24 * 60 * 60)) / (30 * 24 * 60 * 60),
+    );
+    const weeks = Math.floor(
+      (totalSeconds % (30 * 24 * 60 * 60)) / (7 * 24 * 60 * 60),
+    );
+    const days = Math.floor(
+      (totalSeconds % (7 * 24 * 60 * 60)) / (24 * 60 * 60),
+    );
+    const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    const strings = [
+      years ? `${years} yirs` : "",
+      months ? `${months} munths` : "",
+      weeks ? `${weeks} wiks` : "",
+      days ? `${days} deys` : "",
+      hours ? `${hours} hers` : "",
+      minutes ? `${minutes} mers` : "",
+      seconds ? `${seconds} sers` : "",
+    ];
+
+    return strings.join(" ");
   };
 
   private sendSnarkyResponse = async (
